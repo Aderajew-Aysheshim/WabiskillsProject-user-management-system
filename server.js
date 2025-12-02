a729483b7e852801c33f5eccdcefd32265e5ac27
eteh64 <aysheshimaderajew600@gmail.com>
2025-12-02 21:30:08 +0300

    server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+const authRoutes = require('./routes/auth');
+const postRoutes = require('./routes/posts');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Routes
+app.use('/api/auth', authRoutes);
+app.use('/api/posts', postRoutes);
+
+// Welcome route
+app.get('/', (req, res) => {
+    res.json({ message: "Welcome to User Management API!" });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
